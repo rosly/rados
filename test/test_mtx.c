6ac3573ed2e9e6530f9222350b490bfa47bff3c1
Radoslaw Biernacki <rad@semihalf.com>
2015-10-02 18:13:37 +0200

    generic: Adding test for recursive bounded priority inversion

--- test/test_mtx.c
+++ test/test_mtx.c
@@ -51,7 +51,7 @@ static os_task_t task_worker[4];
 static OS_TASKSTACK task_stack[4][OS_STACK_MINSIZE];
 static os_task_t task_coordinator;
 static OS_TASKSTACK coordinator_stack[OS_STACK_MINSIZE];
-static os_mtx_t test_mtx[2];
+static os_mtx_t test_mtx[3];
 static os_sem_t test_sem[3];
 
 static volatile sig_atomic_t test_atomic[2];
@@ -184,14 +184,15 @@ int test_scen2_workerL(void* OS_UNUSED(param))
 
 /**
  * Test scenario:
- * Recursive priority inversion problem
+ * Recursive bounded priority inversion problem
  * To not duplicate description of the problem please refer to Comment 2 in
  * os_mtx.c
  *
  * Task L locks only mtx2
  * Task LN locks both mtx1 and mtx2
  * Task H locks only mtx1
- * Task M does not lock any mtx
+ * Task M is woken up (critical moment)
+ * Test succeeds if task L is scheduled instead task M
  */
 int test_scen3_workerH(void* OS_UNUSED(param))
 {
@@ -281,7 +282,7 @@ int test_scen3_workerL(void* OS_UNUSED(param))
    test_assert(0 == test_atomic[0]); /* verify the valid progress of test state */
    test_atomic[0] = 1;
 
-   /* lock mtx2, switch to Idle and then to LM */
+   /* lock mtx1 */
    ret = os_mtx_lock(&test_mtx[1]);
    test_assert(0 == ret);
 
@@ -321,6 +322,181 @@ int test_scen3_workerL(void* OS_UNUSED(param))
    test_atomic[0] = 14;
    /* also this means that L should now have original prio */
    test_assert(1 == task_worker[3].prio_current);
+
+   return 0;
+}
+
+/**
+ * Test scenario:
+ * Recursive bounded priority inversion problem
+ * Here we focus on os_unlock_mtx() implementation. We check if during call of
+ * this function the task at the end of recursive locking chain gets the right
+ * priority while prio_reset() operation.
+ *
+ * Task L locks mtx0 and mtx1
+ * Task M locks mtx2 and mtx0
+ * Task HM lock mtx1
+ * Task H locks mtx2
+ * Task L unlocks mtx1 (critical moment)
+ * Test succeeds if during os_mtx_unlock() done by task L context is not
+ * switched to task MH.
+ */
+int test_scen4_workerH(void* OS_UNUSED(param))
+{
+   int ret;
+
+   /* block on sem, allow L to progress */
+   ret = os_sem_down(&test_sem[2], OS_TIMEOUT_INFINITE);
+   test_assert(0 == ret);
+
+   /* we will be woken up from L */
+   test_assert(7 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 8;
+
+   /* lock mtx2, this will switch back to L */
+   ret = os_mtx_lock(&test_mtx[2]);
+   test_assert(0 == ret);
+
+   /* we should switch from M */
+   test_assert(11 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 12;
+
+   /* prepare to finish thread */
+   os_mtx_unlock(&test_mtx[2]);
+   test_assert(12 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 13;
+   /* by finishing the thread we should switch to HM */
+
+   return 0;
+}
+
+int test_scen4_workerHM(void* OS_UNUSED(param))
+{
+   int ret;
+
+   /* block on sem, allow L to progress */
+   ret = os_sem_down(&test_sem[1], OS_TIMEOUT_INFINITE);
+   test_assert(0 == ret);
+
+   /* we will be woken up from L */
+   test_assert(5 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 6;
+
+   /* lock mtx1, this will switch back to L */
+   ret = os_mtx_lock(&test_mtx[1]);
+   test_assert(0 == ret);
+
+   /* we should switch from H */
+   test_assert(13 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 14;
+
+   /* prepare to finish thread (no ctx switch this time) */
+   os_mtx_unlock(&test_mtx[1]);
+   test_assert(14 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 15;
+   /* by finishing the thread we should switch to M */
+
+   return 0;
+}
+
+int test_scen4_workerM(void* OS_UNUSED(param))
+{
+   int ret;
+
+   /* block on sem, allow L to progress */
+   ret = os_sem_down(&test_sem[0], OS_TIMEOUT_INFINITE);
+   test_assert(0 == ret);
+
+   /* we will return here from L */
+   test_assert(2 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 3;
+
+   /* lock mtx2 */
+   ret = os_mtx_lock(&test_mtx[2]);
+   test_assert(0 == ret);
+   test_assert(3 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 4;
+
+   /* lock mtx0 this will switch to L */
+   ret = os_mtx_lock(&test_mtx[0]);
+   test_assert(0 == ret);
+
+   /* we should switch from L */
+   test_assert(10 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 11;
+   /* the effective priority of this task should still be on H level */
+   test_assert(4 == task_worker[2].prio_current);
+
+   /* unlock mtx2, there should be context switch to H */
+   os_mtx_unlock(&test_mtx[2]);
+
+   /* we should return from HM */
+   test_assert(15 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 16;
+
+   /* prepare to finish thread (no ctx switch this time) */
+   os_mtx_unlock(&test_mtx[0]);
+   test_assert(16 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 17;
+   /* by finishing the thread we should switch to L */
+
+   return 0;
+}
+
+int test_scen4_workerL(void* OS_UNUSED(param))
+{
+   int ret;
+
+   test_assert(0 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 1;
+
+   /* lock mtx0 and mtx1 */
+   ret = os_mtx_lock(&test_mtx[0]);
+   test_assert(0 == ret);
+   ret = os_mtx_lock(&test_mtx[1]);
+   test_assert(0 == ret);
+
+   test_assert(1 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 2;
+
+   /* switch context to M */
+   os_sem_up(&test_sem[0]);
+
+   /* we will return from M */
+   test_assert(4 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 5;
+
+   /* switch context to HM */
+   os_sem_up(&test_sem[1]);
+
+   /* we will return from HM */
+   test_assert(6 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 7;
+
+   /* switch context to H */
+   os_sem_up(&test_sem[2]);
+
+   /* we will return from H */
+   test_assert(8 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 9;
+
+   /* unlock mtx1, this should not cause switch to HM since we still holds mtx0
+    * on which M is blocked which in turn holds mtx2 on which H is blocked. So
+    * following call should not make context switch since L should remain on
+    * boosted prio = p(H) */
+   os_mtx_unlock(&test_mtx[1]);
+
+   test_assert(9 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 10;
+   /* the effective priority of this task should still be on H level */
+   test_assert(4 == task_worker[3].prio_current);
+
+   /* unlock mtx0, there should be context switch to M */
+   os_mtx_unlock(&test_mtx[0]);
+
+   /* we should switch from M */
+   test_assert(17 == test_atomic[0]); /* verify the valid progress of test state */
+   test_atomic[0] = 18;
 
    return 0;
 }
@@ -395,6 +571,35 @@ int test_coordinator(void* OS_UNUSED(param))
       os_task_join(&task_worker[i]);
    }
    test_assert(14 == test_atomic[0]);
+
+/* scenario 4 */
+   os_taskproc_t scen4_worker_proc[] = {
+	   test_scen4_workerH,
+	   test_scen4_workerHM,
+	   test_scen4_workerM,
+	   test_scen4_workerL
+   };
+   os_sem_create(&test_sem[0], 0);
+   os_sem_create(&test_sem[1], 0);
+   os_sem_create(&test_sem[2], 0);
+   os_mtx_create(&test_mtx[0]);
+   os_mtx_create(&test_mtx[1]);
+   os_mtx_create(&test_mtx[2]);
+   test_atomic[0] = 0;
+   for(i = 0; i < 4; i++)
+   {
+      /* created task will be not scheduled because current task has the highest available priority */
+      os_task_create(
+         &task_worker[i], OS_CONFIG_PRIOCNT - 1 - i,
+         task_stack[i], sizeof(task_stack[i]),
+         scen4_worker_proc[i], (void*)(long)i);
+   }
+   /* scheduler will kick in after following call */
+   for(i = 0; i < 4; i++)
+   {
+      os_task_join(&task_worker[i]);
+   }
+   test_assert(18 == test_atomic[0]);
 
    test_result(0);
    return 0;
